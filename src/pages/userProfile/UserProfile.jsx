a7ee30f364b04fc8048a9adc64e9168f16cdbbcd
KatuGT <93katu@gmail.com>
2023-01-06 21:31:46 -0300

    inicio de perfil usuario

--- src/pages/userProfile/UserProfile.jsx
+++ src/pages/userProfile/UserProfile.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import { WrapperPerfil } from './userProfile.styled';
+import { Button } from '../../components/commonComponents/button/button.jsx';
+import {
+  DataUser,
+  GroupButtons,
+  ImgAndData,
+  ImgUser,
+  OtherData,
+  UserName,
+  WrapperPerfil,
+} from './userProfile.styled';
 
 const UserProfile = () => {
   return (
-    <WrapperPerfil>UserProfile</WrapperPerfil>
+    <WrapperPerfil>
+      <ImgAndData>
+        <ImgUser src="https://imgs.search.brave.com/-ujznuk6JG__TtAtxZHfJ00KrxUz0uncmtz9YiJyJVU/rs:fit:600:600:1/g:ce/aHR0cHM6Ly9zdDQu/ZGVwb3NpdHBob3Rv/cy5jb20vNDMyOTAw/OS8xOTk1Ni92LzQ1/MC9kZXBvc2l0cGhv/dG9zXzE5OTU2NTA3/Mi1zdG9jay1pbGx1/c3RyYXRpb24tY3Jl/YXRpdmUtdmVjdG9y/LWlsbHVzdHJhdGlv/bi1kZWZhdWx0LWF2/YXRhci5qcGc" />
+        <DataUser>
+          <UserName>Katu</UserName>
+          <OtherData>93katu@gmail.com</OtherData>
+          <OtherData>Argentina</OtherData>
+          <OtherData>Mujer</OtherData>
+        </DataUser>
+      </ImgAndData>
+      <GroupButtons>
+        <Button text="Editar" />
+        <Button text="Deshabilitar" />
+        <Button text="Eliminar permanentemente" />
+      </GroupButtons>
+    </WrapperPerfil>
   );
 };
 
--- src/pages/userProfile/userProfile.styled.js
+++ src/pages/userProfile/userProfile.styled.js
@@ -2,7 +2,33 @@ import styled from 'styled-components';
 
 export const WrapperPerfil = styled.div`
     color: #fff;
-    padding: 0%;
-    margin-top: 4rem;
-    background-color: red;
+    margin: 1rem auto;
+    padding: 1rem;
+    width: min(100%, 700px);
+`;
+
+export const ImgAndData = styled.div`
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+`;
+
+export const ImgUser = styled.img`
+    height: 7rem;
+    width: 7rem;
+    border-radius: 100vh;
+    object-fit: cover;
+`;
+
+export const DataUser = styled.div``;
+
+export const UserName = styled.h3`
+    font-size: 1.5rem;
+`;
+
+export const OtherData = styled.p``;
+
+export const GroupButtons = styled.div`
+    display: flex;
+    flex-wrap: wrap;
 `;
